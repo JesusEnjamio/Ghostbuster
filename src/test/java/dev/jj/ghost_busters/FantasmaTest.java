05d890e399768748d0baba4611ea85b8a8e1c81e
JoseRD149 <joseromerodiaz89@gmail.com>
2025-02-06 14:43:35 +0100

    test constructor Fantasma doesnt work

--- src/test/java/dev/jj/ghost_busters/FantasmaTest.java
+++ src/test/java/dev/jj/ghost_busters/FantasmaTest.java
@@ -1,4 +1,21 @@
 package dev.jj.ghost_busters;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.time.LocalDateTime;
 
 public class FantasmaTest {
+    @Test
+public void testConstructorInicializaAtributosCorrectamente() {
+    int id = 1;
+    String nombre = "Casper";
+    String clase = "Clase I";
+    String nivelPeligro = "Bajo";
+    String habilidad = "Invisibilidad";
+
+    Fantasma fantasma = new Fantasma(id, nombre, clase, nivelPeligro, habilidad);
+
+    
+}
 }
