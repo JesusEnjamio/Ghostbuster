3bda942d5a5a30337f07f237057843901cdcbe40
JoseRD149 <joseromerodiaz89@gmail.com>
2025-02-06 15:11:25 +0100

    testGetters doesnt work

--- src/test/java/dev/jj/ghost_busters/FantasmaTest.java
+++ src/test/java/dev/jj/ghost_busters/FantasmaTest.java
@@ -1,4 +1,5 @@
 package dev.jj.ghost_busters;
+
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -7,15 +8,32 @@ import java.time.LocalDateTime;
 
 public class FantasmaTest {
     @Test
-public void testConstructor() {
-    int id = 1;
-    String nombre = "Casper";
-    String clase = "Clase I";
-    String nivelPeligro = "Bajo";
-    String habilidad = "Invisibilidad";
+    public void testConstructor() {
+        int id = 1;
+        String nombre = "Casper";
+        String clase = "Clase I";
+        String nivelPeligro = "Bajo";
+        String habilidad = "Invisibilidad";
 
-  
-       // Assert
-      
-}
+        Fantasma fantasma = new Fantasma(id, nombre, clase, nivelPeligro, habilidad);
+
+    }
+
+    @Test
+    public void testGetters() {
+        int id = 1;
+        String nombre = "Casper";
+        String clase = "Clase I";
+        String nivelPeligro = "Bajo";
+        String habilidad = "Invisibilidad";
+
+        Fantasma fantasma = new Fantasma(id, nombre, clase, nivelPeligro, habilidad);
+
+        assertThat(fantasma.getId(), equalTo(id));
+        assertThat(fantasma.getNombre(), equalTo(nombre));
+        assertThat(fantasma.getClase(), equalTo(clase));
+        assertThat(fantasma.getNivelPeligro(), equalTo(nivelPeligro));
+        assertThat(fantasma.getHabilidad(), equalTo(habilidad));
+    }
+
 }
